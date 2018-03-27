5063c1e574827fb1d5af3dd5ea3481d6da408589
Dezzles <daniel@dezzles.com>
2018-03-28 07:47:19 +1100

    Add tests and package

---
+++ test/point.spec.js
@@ -0,0 +1,13 @@
+const {expect} = require('chai');
+let { Point } = require('../quadtree');
+
+describe('Point', () => {
+  it('assigns x on creation', () => {
+    let point = new Point(12, 23);
+    expect(point.x).to.equal(12);
+  });
+  it('assigns y on creation', () => {
+    let point = new Point(12, 23);
+    expect(point.y).to.equal(23);
+  });
+});
