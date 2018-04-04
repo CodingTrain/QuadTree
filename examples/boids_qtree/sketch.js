66291a79f0513c2da3c906f4e5c46c25ce9f39d7
WakeMeAtThree <wakemeatthree@gmail.com>
2018-04-04 21:13:49 +0400

    Added sliders and checkboxes to interface with flock sketch

--- examples/boids_qtree/sketch.js
+++ examples/boids_qtree/sketch.js
@@ -13,23 +13,29 @@ let boundary;
 let count = 0;
 let parameters;
 
+let _display_quadtree;
+let _background;
+let framerateP;
+
+
 function setup() {
-    createCanvas(600, 400);
+    cnv = createCanvas(600, 400);
     background(0);
     smooth()
     colorMode(HSB)
 
     // Parameters object
-    parameters = {boids: 600,
-                  sep: 1.5,
-                  ali: 1,
-                  coh: 1,
-                  sep_tolerance: 50,
-                  ali_tolerance: 25,
-                  coh_tolerance: 25,
-                  display_qtree: true,
-                  background: true
-                };
+    parameters = {
+        boids: 700,
+        sep: 1.5,
+        ali: 1,
+        coh: 1,
+        sep_tolerance: 50,
+        ali_tolerance: 25,
+        coh_tolerance: 25,
+        display_qtree: true,
+        background: false
+    };
 
     // Instantiate boids and quadtree boundary
     for (let i = 0; i < parameters.boids; i++) {
@@ -37,35 +43,55 @@ function setup() {
     }
 
     boundary = new Rectangle(width / 2, height / 2, width / 2, height / 2);
+
+    // Interface Setup
+    _display_qtree = createCheckbox('Display quadtree');
+    _display_qtree.checked(true);
     
+    _background = createCheckbox('Display background (Paint!)');
+    _background.checked(true);
+
+    let totalP = createP(parameters.boids);
+    total = createSlider(1, 1000, parameters.boids);
+    total.input(function () {
+        parameters.boids = total.value();
+        totalP.html(parameters.boids);
+        while (parameters.boids > particles.length) {
+            particles.push(new Boid(parameters));
+        }
+        if (parameters.boids < particles.length) {
+            particles.splice(0, particles.length - parameters.boids);
+        }
+    });
 }
 
 function draw() {
+
     //Switch off background and render with alpha for so much fun 
-    if(parameters.background) background(0);
-    
+    if (_background.checked()) background(0);
+
     // Instantiate a quadtree from particles at every frame
     let qtree = new QuadTree(boundary, 4);
-    
+
     for (let p of particles) {
         let point = new Point(p.loc.x, p.loc.y, p);
         qtree.insert(point)
-      }
-     
+    }
+
     //Run particle system here
     for (let p of particles) {
         p.run(qtree);
     }
-    
+
     // Display quadtree in action
-    if(parameters.display_qtree) show(qtree);
+    if (_display_qtree.checked()) show(qtree);
 
 }
 
 
 function show(qtree) {
     //Alternate color of quad stroke to show it well for background = false
-    _color = color(303,56,25+25*sin(frameCount/50))
+    _color = color(303, 56, 25 + 25 * sin(frameCount / 50))
     stroke(_color);
 
     noFill();
@@ -74,9 +100,9 @@ function show(qtree) {
     rect(qtree.boundary.x, qtree.boundary.y, qtree.boundary.w * 2, qtree.boundary.h * 2);
 
     if (qtree.divided) {
-      show(qtree.northeast);
-      show(qtree.northwest);
-      show(qtree.southeast);
-      show(qtree.southwest);
+        show(qtree.northeast);
+        show(qtree.northwest);
+        show(qtree.southeast);
+        show(qtree.southwest);
     }
-  }
+}
