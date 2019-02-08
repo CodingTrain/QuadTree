837443977243d150e1e25d952b7e64c3ccb35228
Chris Hallberg <crhallberg@gmail.com>
2019-02-08 00:44:17 -0500

    Allow specifying a starting size for use-case optimization.

--- quadtree.js
+++ quadtree.js
@@ -165,6 +165,9 @@ class QuadTree {
     if (typeof count === "undefined") {
       count = 1;
     }
+    if (typeof startingSize === "undefined") {
+      startingSize = 1;
+    }
 
     if (!this.divided) {
       // Empty
@@ -179,7 +182,7 @@ class QuadTree {
 
     // optimized, expanding binary search
     // start with a small circle, rapidly expand, slowly shrink
-    let radius = 1;
+    let radius = startingSize;
     let limit = 16;
     while (true) {
       const range = new Circle(point.x, point.y, radius);
